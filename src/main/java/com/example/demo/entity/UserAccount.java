d112f2cbffde4aa4af5631dce7770786ed20da39
bala-amypo <bala@amypo.in>
2025-12-21 18:56:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,7 +1,6 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.Instant;
 
 @Entity
 public class UserAccount {
@@ -11,19 +10,17 @@ public class UserAccount {
     private Long id;
 
     private String fullName;
-
-    @Column(unique = true)
     private String email;
-
     private String password;
-    private String role = "USER";
+    private String role;
 
-    private Instant createdAt;
+    public Long getId() { return id; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
 
-    @PrePersist
-    void onCreate() {
-        createdAt = Instant.now();
-    }
-
-    // getters and setters
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setRole(String role) { this.role = role; }
 }
