8ec81b9d565740e9b2616e238ca7ac85e3f7bcea
bala-amypo <bala@amypo.in>
2025-12-21 18:26:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/SupplierRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+    // REQUIRED by tests
+    List<Supplier> findByIsActiveTrue();
+}
